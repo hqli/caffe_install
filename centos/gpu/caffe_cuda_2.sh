03705d885b04f6eed58f333a73328141aa447b29
hqli <linkhqli@163.com>
2016-05-23 03:56:16 +0000

    Update caffe_cuda_2.sh

--- centos/gpu/caffe_cuda_2.sh
+++ centos/gpu/caffe_cuda_2.sh
@@ -2,11 +2,20 @@
 
 #Set BIOS start only with UMA
 #ctrl+Alt+F1 
-sudo yum install kernel-devel kernel-headers
+
 sudo init 3
 
 sudo chmod +x cuda_7.5.18_linux.run
 sudo ./cuda_7.5.18_linux.run
+
+#CUDA7.5 configure
+sudo sh -c 'echo "export PATH=/usr/local/cuda-7.5/bin:\$PATH" >> /etc/profile'
+
+source /etc/profile
+
+sudo sh -c 'echo "/usr/local/cuda-7.5/lib64" > /etc/ld.so.conf.d/cuda.conf'
+
+sudo ldconfig
 
 #then reboot
 
