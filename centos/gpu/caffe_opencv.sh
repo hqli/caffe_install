277e55bed17e4c74f0ac3be4eaeba90da99a9393
hqli <linkhqli@163.com>
2016-05-23 19:00:58 +0800

    opencv

--- centos/gpu/caffe_opencv.sh
+++ centos/gpu/caffe_opencv.sh
@@ -11,10 +11,15 @@ cd opencv-2.4.9
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
 
-sed  -i '/string(MD5/d' cmake/cl2cpp.cmake
+cmake --version | grep " 2.6"
+if [ $? -eq 0 ]; then
+    # Delete lines beginning with string(MD5 based on incompatibility with cmake 2.6.  See 
+    # http://answers.opencv.org/question/24095/building-opencv-247-on-centos-6/
+    sed  -i '/string(MD5/d' cmake/cl2cpp.cmake
+fi
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_CUDA ..
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local  ..
 make -j 4
 sudo make install
 cd ../..
