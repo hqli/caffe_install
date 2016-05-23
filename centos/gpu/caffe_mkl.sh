fa03c7f6ea127d3915da75ce90a82788e2f29c55
hqli <linkhqli@163.com>
2016-05-23 03:56:59 +0000

    Update caffe_mkl.sh

--- centos/gpu/caffe_mkl.sh
+++ centos/gpu/caffe_mkl.sh
@@ -17,3 +17,8 @@ sudo yum install -y xulrunner.i686
 tar xvf parallel_studio_xe_2015_update3.tgz
 cd parallel_studio_xe_2015_update3
 sudo sh install.sh
+
+# mkl config
+sudo sh -c 'echo "/opt/intel/lib/intel64" > /etc/ld.so.conf.d/mkl.conf'
+sudo sh -c 'echo "/opt/intel/mkl/lib/intel64" >> /etc/ld.so.conf.d/mkl.conf'
+sudo ldconfig
